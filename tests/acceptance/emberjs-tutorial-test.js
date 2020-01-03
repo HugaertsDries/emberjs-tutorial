19fc1406cde5087c418bd8f08199638f09a19e45
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-01-03 14:09:11 +0100

    updated acceptence-tests to test the navigation

--- tests/acceptance/emberjs-tutorial-test.js
+++ tests/acceptance/emberjs-tutorial-test.js
@@ -12,6 +12,8 @@ module('Acceptance | emberjs tutorial', function (hooks) {
     await visit('/');
 
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Welcome to Super Rentals!');
 
     assert.dom('.jumbo a.button').hasText('About Us');
@@ -24,6 +26,8 @@ module('Acceptance | emberjs tutorial', function (hooks) {
     await visit('/about');
 
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('About Super Rentals');
 
     assert.dom('.jumbo a.button').hasText('Contact Us')
@@ -36,11 +40,31 @@ module('Acceptance | emberjs tutorial', function (hooks) {
     await visit('/getting-in-touch');
 
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('.jumbo a.button').hasText('About')
     await click('.jumbo a.button')
 
     assert.equal(currentURL(), '/about')
+  });
+
+  test('navigating using the nav-bar', async function(assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals')
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
   });
 });
